1c0d5c615212f6675f87f40551185f92999e2718
Joel Jorge Nunes Vazquez <jnunesvazquez@danielcastelao.org>
2020-12-17 18:23:11 +0100

    Codigo con la instancia para el Singleton

--- src/com/cod/Auxiliar.java
+++ src/com/cod/Auxiliar.java
@@ -4,6 +4,9 @@ public class Auxiliar {
     String email;
     String ip = "127.0.0.2";
 
+    //esta va a ser la unica instancia
+    static Auxiliar instance = null;
+
     Auxiliar(){
         email = "anonymous@danielcastelao.org";
     }
@@ -12,6 +15,16 @@ public class Auxiliar {
         this.email = parametro1;
     }
 
+    public static Auxiliar getInstance() {
+        // compruebo si ya está creada
+        if (instance == null) {
+            // como no está creada, la creo
+            // desde aqui si que puedo acceder al constructor
+            instance = new Auxiliar();
+        }
+        // devuelvo la instancia
+        return instance;
+    }
     public boolean con() {
         if (email != "anonymous@danielcastelao.org") {
             return true;
